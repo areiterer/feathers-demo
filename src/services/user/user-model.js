c6f77cf63142d6092a777ff4faaef49fb785824d
Andreas Reiterer <reiterer.a@gmail.com>
2017-02-06 20:26:53 +0100

    Add fields for verify/resetPassword to user model

--- src/services/user/user-model.js
+++ src/services/user/user-model.js
@@ -11,7 +11,14 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: {type: String, required: true, unique: true},
   password: { type: String, required: true },
-  
+
+  isVerified: { type: Boolean },
+  verifyToken: { type: String },
+  verifyExpires: { type: Date },
+  verifyChanges: { type: Object },
+  resetToken: { type: String },
+  resetExpires: { type: Date },
+
   createdAt: { type: Date, 'default': Date.now },
   updatedAt: { type: Date, 'default': Date.now }
 });
