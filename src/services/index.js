b30b09ed198daa9151a4080839fc5f395d82ed21
Andreas Reiterer <reiterer.a@gmail.com>
2017-02-06 20:05:50 +0100

    Add new service for sending mails

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const email = require('./email');
 const article = require('./article');
 const authentication = require('./authentication');
 const user = require('./user');
@@ -12,4 +13,5 @@ module.exports = function() {
   app.configure(authentication);
   app.configure(user);
   app.configure(article);
+  app.configure(email);
 };
